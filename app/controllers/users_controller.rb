015f35f5772c3c82506659b0c1def70fef1e53ab
Stephen McCaffery <stephen.mccaffery@kellogg.northwestern.edu>
2025-05-20 14:37:25 +0000

    save at 1st break

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,15 +4,18 @@ class UsersController < ApplicationController
   end
 
   def new
+    #render :template => 'users/new'
   end
 
   def create
     @user = User.new
-    @user["first_name"] = params["first_name"]
-    @user["last_name"] = params["last_name"]
-    @user["email"] = params["email"]
-    @user["password"] = params["password"]
-    @user.save
+    # if params["email"].inlcude?("@") -- #how do assure entered in email format, and only save if so
+      @user["first_name"] = params["first_name"]
+      @user["last_name"] = params["last_name"]
+      @user["email"] = params["email"]
+      @user["password"] = params["password"]
+      @user.save
+    # end
     redirect_to "/users/#{@user["id"]}"
   end
 end
